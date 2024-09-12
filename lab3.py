651b5fe3e898d15f1b377f84092f15fba1fef6c9
jimiller00 <156855608+jimiller00@users.noreply.github.com>
2024-09-12 16:07:38 +0000

    lab3v1.0

--- lab3.py
+++ lab3.py
@@ -5,26 +5,28 @@ from openai import OpenAI
  # Show title and description.
 st.title("John's Chat Bot")    
 
-OpenAI_model = st.sidebar.selectbox("Which model?", "Mini", "Regular")
+OpenAI_model = st.sidebar.selectbox("Which model?", ("Mini", "Regular"))
 
 if OpenAI_model == "Mini":
     model_to_use = "gpt-4o-mini"
 else:
     model_to_use = "gpt-4o"
+
 # Create an OpenAI client.
 if 'client' not in st.session_state:
     api_key = st.secrets["openai_key"]
     st.session_state.client = OpenAI(api_key=api_key)
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = \
-        [{"role": "assistant", "content": "How can I help you?"}]
+         [{"role": "assistant", "content": "How can I help you?"}]
     
-#
+# 
 for msg in st.session_state.messages:
     chat_msg = st.chat_message(msg["role"])
     chat_msg.write(msg["content"])
 
-#
+# user inputs prompt
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -34,8 +36,12 @@ if prompt := st.chat_input("What is up?"):
     client = st.session_state.client
     stream = client.chat.completions.create(
         model=model_to_use,
-        messages = st.session_state.messsages,
+        messages = st.session_state.messages,
         stream=True)
+    
+    # display assistant response in chat message container
     with st.chat_message("assistant"):
         response = st.write_stream(stream)
-    st.session_state.append({"role": "assistant", "content": response})
+
+    # add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
